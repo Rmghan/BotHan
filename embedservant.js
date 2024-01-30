3e2960734b8bbc5c706073426fb5425d049ec706
rmghan <rmghan@gmail.com>
2024-01-30 14:07:15 +0500

    removed some spaces for better viewing

--- embedservant.js
+++ embedservant.js
@@ -44,10 +44,10 @@ function getskillfunctions(skill) {
 
 	for (let i = 0; i < skill.functions.length; i++) {
 		if (skill.functions[i].svals[0].Value === undefined && skill.functions[i].funcTargetTeam !== 'enemy') {
-			replyText += `\n ${skill.functions[i].funcPopupText} `;
+			replyText += `\n${skill.functions[i].funcPopupText} `;
 		}
 		else if (skill.functions[i].funcTargetTeam !== 'enemy') {
-			replyText += `\n ***${skill.functions[i].funcPopupText}*** : ${skill.functions[i].svals[0].Value}/${skill.functions[i].svals[9].Value}`;
+			replyText += `\n***${skill.functions[i].funcPopupText}*** : ${skill.functions[i].svals[0].Value}/${skill.functions[i].svals[9].Value}`;
 		}
 	}
 	return {
@@ -57,16 +57,16 @@ function getskillfunctions(skill) {
 function getNoblePhantasm(NP) {
 	let replyText = '__***\n Noble Phantasm:***__';
 	for (let i = 0; i < NP.length; i++) {
-		replyText += `\n\n\n**NP Type**: *${NP[i].card.toUpperCase()}*\n **Details**: __${NP[i].detail}__`;
+		replyText += `\n\n\n**NP Type**: *${NP[i].card.toUpperCase()}*\n**Details**: __${NP[i].detail}__`;
 		for (let j = 0; j < NP[i].functions.length; j++) {
 			if (NP[i].functions[j].svals[0].Value === undefined && NP[i].functions[j].funcTargetTeam !== 'enemy') {
-				replyText += `\n **${NP[i].functions[j].funcPopupText}**`;
+				replyText += `\n**${NP[i].functions[j].funcPopupText}**`;
 				if (NP[i].functions[j].svals[0].Count !== -1 && NP[i].functions[j].svals[0].Count !== undefined) {
 					replyText += `(${NP[i].functions[j].svals[0].Count}/${NP[i].functions[j].svals2[1].Count}/${NP[i].functions[j].svals3[2].Count}/${NP[i].functions[j].svals4[3].Count}/${NP[i].functions[j].svals5[4].Count})`;
 				}
 			}
 			else if (NP[i].functions[j].funcTargetTeam !== 'enemy') {
-				replyText += `\n **${NP[i].functions[j].funcPopupText}**: *${NP[i].functions[j].svals[0].Value}/${NP[i].functions[j].svals2[1].Value}/${NP[i].functions[j].svals3[2].Value}/${NP[i].functions[j].svals4[3].Value}/${NP[i].functions[j].svals5[4].Value}*`;
+				replyText += `\n**${NP[i].functions[j].funcPopupText}**: *${NP[i].functions[j].svals[0].Value}/${NP[i].functions[j].svals2[1].Value}/${NP[i].functions[j].svals3[2].Value}/${NP[i].functions[j].svals4[3].Value}/${NP[i].functions[j].svals5[4].Value}*`;
 				if (NP[i].functions[j].svals[0].Count !== -1 && NP[i].functions[j].svals[0].Count !== undefined) {
 					replyText += `(${NP[i].functions[j].svals[0].Count}/${NP[i].functions[j].svals2[1].Count}/${NP[i].functions[j].svals3[2].Count}/${NP[i].functions[j].svals4[3].Count}/${NP[i].functions[j].svals5[4].Count})`;
 				}
