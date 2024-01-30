ca0222fd16b419aefbdb2ddd615087cfb8e1da1b
rmghan <rmghan@gmail.com>
2024-01-30 11:42:47 +0500

    Added basic Noble Phantasm code, will need to update with better algo later, currently Space ishtar is unsearchable

--- embedservant.js
+++ embedservant.js
@@ -1,51 +1,35 @@
 let embedservant = null;
 
-async function makeEmbedServant(x) {
-	const skill1 = x.skills.findLast(e => e.num === 1);
-	const skill2 = x.skills.findLast(e => e.num === 2);
-	const skill3 = x.skills.findLast(e => e.num === 3);
-	const skill1functions = await getskillfunctions(skill1);
-	const skill2functions = await getskillfunctions(skill2);
-	const skill3functions = await getskillfunctions(skill3);
-
+function makeEmbedServant(servant) {
 	embedservant = {
-		title:  x.name,
-		thumbnail: { url: x.extraAssets.faces.ascension[2] },
-		description: `${x.rarity}* ${(x.className).toUpperCase()}`,
+		title:  servant.name,
+		thumbnail: { url: servant.extraAssets.faces.ascension[2] },
+		description: `${servant.rarity}* ${(servant.className).toUpperCase()}`,
 		fields: [
 			{
 				name : 'Servant Atk:',
-				value: `${x.atkBase}/${x.atkMax}`,
+				value: `${servant.atkBase}/${servant.atkMax}`,
 				inline: true,
 			},
 			{
 				name: 'Servant HP:',
-				value: `${x.hpBase}/${x.hpMax}`,
+				value: `${servant.hpBase}/${servant.hpMax}`,
 				inline: true,
 
 			},
 			{
 				name: 'Attribute:',
-				value: x.attribute,
+				value: servant.attribute,
 				inline: true,
 			},
 			{
 				name:  '\u200B',
 				value: '\u200B',
 			},
-			{
-				name: skill1.name,
-				value: `Details: ${skill1.detail} \n ${ skill1functions.map(i => (`\n ${i.functionName}(${i.functionTeam}): ${i.functionMinVal}/${i.functionMaxVal}`))} `,
-			},
-			{
-				name: skill2.name,
-				value: `Details: ${skill2.detail} \n ${ skill2functions.map(i => (`\n ${i.functionName}(${i.functionTeam}): ${i.functionMinVal}/${i.functionMaxVal}`))} `,
-			},
-			{
-				name: skill3.name,
-				value: `Details: ${skill3.detail} \n ${ skill3functions.map(i => (`\n ${i.functionName}(${i.functionTeam}): ${i.functionMinVal}/${i.functionMaxVal}`))}`,
-			},
-
+			getskillfunctions(servant.skills.findLast(e => e.num === 1)),
+			getskillfunctions(servant.skills.findLast(e => e.num === 2)),
+			getskillfunctions(servant.skills.findLast(e => e.num === 3)),
+			getNoblePhantasm(servant.noblePhantasms),
 
 		],
 
@@ -55,11 +39,45 @@ async function makeEmbedServant(x) {
 
 }
 
-async function getskillfunctions(x) {
-	const skillfunctions = x.functions.map(i => ({ functionName: i.funcPopupText, functionTeam: i.funcTargetTeam, functionMinVal: i.svals[0].Value, functionMaxVal: i.svals[9].Value }));
-	return skillfunctions;
-}
+function getskillfunctions(skill) {
+	let replyText = `__${skill.detail}__ \n`;
 
+	for (let i = 0; i < skill.functions.length; i++) {
+		if (skill.functions[i].svals[0].Value === undefined && skill.functions[i].funcTargetTeam !== 'enemy') {
+			replyText += `\n ${skill.functions[i].funcPopupText} `;
+		}
+		else if (skill.functions[i].funcTargetTeam !== 'enemy') {
+			replyText += `\n ***${skill.functions[i].funcPopupText}*** : ${skill.functions[i].svals[0].Value}/${skill.functions[i].svals[9].Value}`;
+		}
+	}
+	return {
+		name: skill.name,
+		value: replyText };
+}
+function getNoblePhantasm(NP) {
+	let replyText = '__***\n Noble Phantasm:***__';
+	for (let i = 0; i < NP.length; i++) {
+		replyText += `\n\n\n**NP Type**: *${NP[i].card.toUpperCase()}*\n **Details**: __${NP[i].detail}__`;
+		for (let j = 0; j < NP[i].functions.length; j++) {
+			if (NP[i].functions[j].svals[0].Value === undefined && NP[i].functions[j].funcTargetTeam !== 'enemy') {
+				replyText += `\n **${NP[i].functions[j].funcPopupText}**`;
+				if (NP[i].functions[j].svals[0].Count !== -1 && NP[i].functions[j].svals[0].Count !== undefined) {
+					replyText += `(${NP[i].functions[j].svals[0].Count}/${NP[i].functions[j].svals2[1].Count}/${NP[i].functions[j].svals3[2].Count}/${NP[i].functions[j].svals4[3].Count}/${NP[i].functions[j].svals5[4].Count})`;
+				}
+			}
+			else if (NP[i].functions[j].funcTargetTeam !== 'enemy') {
+				replyText += `\n **${NP[i].functions[j].funcPopupText}**: *${NP[i].functions[j].svals[0].Value}/${NP[i].functions[j].svals2[1].Value}/${NP[i].functions[j].svals3[2].Value}/${NP[i].functions[j].svals4[3].Value}/${NP[i].functions[j].svals5[4].Value}*`;
+				if (NP[i].functions[j].svals[0].Count !== -1 && NP[i].functions[j].svals[0].Count !== undefined) {
+					replyText += `(${NP[i].functions[j].svals[0].Count}/${NP[i].functions[j].svals2[1].Count}/${NP[i].functions[j].svals3[2].Count}/${NP[i].functions[j].svals4[3].Count}/${NP[i].functions[j].svals5[4].Count})`;
+				}
+
+			}
+
+		}
+	}
+	return { name: NP[0].name,
+		value: replyText };
+}
 module.exports = {
 	makeEmbedServant,
 
