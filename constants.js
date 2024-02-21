ecdc4acff7007a9e1cc82d93afeddf9c514da302
rmghan <rmghan@gmail.com>
2024-02-22 03:58:03 +0500

    added HP loss to constants

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const buffNameList = [ 'Guts', 'C. Star Gain\nPer Turn', 'Damage Cut', 'Max HP Plus', 'HP Recovery\nPer Turn', 'C. Star Gain', '', 'Command Card Type Change', 'ATK Plus'];
+const buffNameList = [ 'Guts', 'C. Star Gain\nPer Turn', 'HP Loss', 'Damage Cut', 'Max HP Plus', 'HP Recovery\nPer Turn', 'C. Star Gain', '', 'Command Card Type Change', 'ATK Plus'];
 const npGainBuff = ['NP Gain', 'NP Gain\nPer Turn'];
 const servantStateIDs = ['304800'];
 
