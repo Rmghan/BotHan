8198cb24ded881cac4e978528e3729d822250ce0
rmghan <rmghan@gmail.com>
2024-02-24 23:47:24 +0500

    got rid of a bunch of spaces for more compact code, hope I don't regret it later.

--- embedMC.js
+++ embedMC.js
@@ -8,20 +8,10 @@ async function makeEmbedMC(Mystic_Code) {
 		title:  Mystic_Code.name,
 		thumbnail: { url: Mystic_Code.extraAssets.item.male },
 		description: `${Mystic_Code.detail}`,
-		fields: [
-			...getSkillFunctions(Mystic_Code.skills.filter(e => e.num === 0)),
-
-
-		],
-
-
+		fields: [...getSkillFunctions(Mystic_Code.skills.filter(e => e.num === 0))],
 	};
 	return embedobject;
-
 }
 
-module.exports = {
-	makeEmbedMC,
-
-};
+module.exports = { makeEmbedMC };
 
